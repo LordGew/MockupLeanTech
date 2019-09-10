5bf6ff5a75bdafa39a45b426a9a2ce4491b277fb
Jairo Hernandez <jairo.hernandez1@sykes.com>
2019-09-10 17:19:34 -0500

    inicio de bakcup

--- src/app/Imports/material-Imports.ts
+++ src/app/Imports/material-Imports.ts
@@ -3,7 +3,10 @@ import { CommonModule } from '@angular/common';
 import {MatFormFieldModule} from '@angular/material/form-field'; 
 import {MatCardModule} from '@angular/material/card'; 
 import {MatTabsModule} from '@angular/material/tabs'; 
-import {MatSidenavModule} from '@angular/material/sidenav'; 
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatGridListModule} from '@angular/material/grid-list'; 
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatIconModule} from '@angular/material/icon'; 
 
 @NgModule({
 imports: [  
@@ -12,6 +15,10 @@ imports: [
     MatCardModule,
     MatTabsModule,
     MatSidenavModule,
+    MatGridListModule,
+    MatSlideToggleModule,
+    MatIconModule,
+    
  ],
 declarations: [ ],
 
@@ -20,6 +27,10 @@ exports:[
     MatCardModule,
     MatTabsModule,
     MatSidenavModule,
+    MatGridListModule,
+    MatSlideToggleModule,
+    MatIconModule,
+    
 ]  
 })
 export class MaterialImports{}
